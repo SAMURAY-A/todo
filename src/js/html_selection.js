d6a5679e2d1dbabb952d6e9ef9b7ac2f19db951b
SAMURAY-A <abdullohaziz559@gmail.com>
2024-12-04 10:45:06 +0500

    first commit

---
+++ src/js/html_selection.js
@@ -0,0 +1,4 @@
+const elTodosParent = document.getElementById("todosParent");
+const elTodosTemplate = document.getElementById("todoTemplate");
+const eltodoForm = document.getElementById("todoForm");
+export { elTodosParent, elTodosTemplate, eltodoForm };
